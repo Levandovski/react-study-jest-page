51640c8adb43c215e32f2dd3a199492abde4661a
levandovski <jesse.levandovski@outlook.com>
2021-03-16 23:04:15 -0300

    Projeto para estudar testes

---
+++ src/App.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import { App } from './App';
+
+
+
+describe('Testando Página', () => {
+    
+    //afterEach(cleanup);
+
+    it('Teste1', () =>{
+        const { getByText, getByTestId } = render(<App/>);
+        expect(getByText('Bem Vindo ao Site!')).toBeInTheDocument();
+    });
+})
